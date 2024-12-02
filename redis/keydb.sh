4698c0c28bd8f2c6c80988c73f499c61407b20ad
Harrison | MSI | Linux <hchumpitaz92@gmail.com>
2024-12-02 10:27:47 -0500

    feat: Se genera un archivo de configuración de keydb.conf: • Se crea usuario de odoo Se establecen los parametros: • requirepass=REDIS_PASSWORD • appendonly=REDIS_PERSISTENT_STORAGE • server-threads=REDIS_THREADS

---
+++ redis/keydb.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e
+
+# Define the path to the example configuration file
+TEMPLATE_CONF="keydb.conf"
+
+# First pass: Evaluate any nested variables within .env file and export them
+while IFS='=' read -r key value || [[ -n $key ]]; do
+    # Skip comments and empty lines
+    [[ $key =~ ^#.* ]] || [[ -z $key ]] && continue
+    
+    # Removing any quotes around the value
+    value=${value%\"}
+    value=${value#\"}
+    
+    # Evaluate any variables within value
+    eval "value=\"$value\""
+    
+    export "$key=$value"
+done < .env
+
+# Copy the example conf to the destination to start replacing the variables
+cp "$TEMPLATE_CONF" "$KEYDB_CONFIG"
+
+# Second pass: Replace the variables in $KEYDB_CONFIG
+while IFS='=' read -r key value || [[ -n $key ]]; do
+    # Skip comments and empty lines
+    [[ $key =~ ^#.* ]] || [[ -z $key ]] && continue
+    
+    value=${!key} # Get the value of the variable whose name is $key
+    
+    # Escape characters which are special to sed
+    value_escaped=$(echo "$value" | sed 's/[\/&]/\\&/g')
+    
+    # Replace occurrences of the key with the value in $KEYDB_CONFIG
+    sed -i "s/\${$key}/${value_escaped}/g" "$KEYDB_CONFIG"
+done < .env
+
+echo "Configuration file is generated at $KEYDB_CONFIG"
